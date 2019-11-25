633a61191eff1b52807948ce340e84ea533c2aeb
Ranika Williams <rnkprice07@gmail.com>
2019-11-25 14:43:57 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,9 +12,9 @@ class Post < ActiveRecord::Base
     where("created_at >=?", Time.zone.today.beginning_of_day)
   end
  
-def self.old_news
+  def self.old_news
     where("created_at <?", Time.zone.today.beginning_of_day)
-end
+  end
 
   private
 
